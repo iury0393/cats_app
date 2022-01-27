67421a186b733b5cc2d9e11af2c5b1cb11e81e88
Iury Vasconcelos <iury0393@gmail.com>
2022-01-27 18:06:20 -0300

    Adicionando o splash page e analysis

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:cats/app/modules/splash/splash_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'modules/home/home_module.dart';
@@ -8,7 +9,7 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => SplashPage()),
+    ModuleRoute('/home', module: HomeModule()),
   ];
-
 }
