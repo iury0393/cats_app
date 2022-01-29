4a97bf1020cc46cf177662154de9875eb0fde485
Iury Vasconcelos <iury0393@gmail.com>
2022-01-29 11:01:51 -0300

    Adicionando as imagens

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
-  final String title;
-  const SplashPage({Key? key, this.title = 'SplashPage'}) : super(key: key);
   @override
   SplashPageState createState() => SplashPageState();
 }
+
 class SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    setupInitializers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
+      body: Stack(
         children: <Widget>[],
       ),
     );
   }
+
+  setupInitializers() {}
 }
