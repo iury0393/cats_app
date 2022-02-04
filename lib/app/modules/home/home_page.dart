4c9c7a900e24c37fae28744a6fac5b9aaca2de34
Iury Vasconcelos <iury0393@gmail.com>
2022-02-04 10:53:18 -0300

    Criando o esqueleto da home

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cats/app/shared/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -13,13 +14,64 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           'CAATS',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
         ),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: Utils.getPaddingDefault(),
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            Container(
+              width: Utils.displayWidth(context, reducedBy: 50),
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            SizedBox(height: 15),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 6,
+                children: [
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                  Card(
+                    elevation: 9,
+                    color: Colors.purple,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
